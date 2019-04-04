ac2ace4d2d2c1a991cf974dd071bf031fe06a0e5
maomao1996 <1714487678@qq.com>
2019-04-04 18:03:30 +0800

    feat：升级 Vue 版本，优化脚手架配置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'babel-polyfill'
-import 'assets/js/hack'
+// import 'assets/js/hack'
 import Vue from 'vue'
 import store from './store'
 import router from './router'
