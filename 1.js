84f230244d474a8f149c01ff8923afae41d42e28
shaonianzhentan <ilovevar@gmail.com>
2019-07-07 10:31:01 +0800

    升级

--- 1.js
+++ 1.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
 
 let today = new Date()
-
-fs.writeFileSync('./dist/config.json', JSON.stringify({
+let config = {
   ver: `${today.getFullYear()}.${today.getMonth() + 1}.${today.getDate()}.${today.getHours()}.${today.getMinutes()}`
-}))
-fs.writeFileSync('./dist/cloud-music.js', '')
+}
+fs.writeFileSync('./dist/config.json', JSON.stringify(config))
+
+let content = fs.readFileSync('./template.js', 'utf-8')
+
+fs.writeFileSync('./dist/cloud-music.js',
+  content.replace('custom-localhost', `/community_plugin/lovelace-cloud-music/index.html?ver=${config.ver}`))
+
