df0d5bc3a5bdada36b2c84cec9409502e48813f1
maomao1996 <1714487678@qq.com>
2020-04-18 21:23:00 +0800

    refactor：优化 toHttps 方法和版本更新时间的写入

--- src/utils/util.js
+++ src/utils/util.js
@@ -93,7 +93,15 @@ export function silencePromise(value) {
   }
 }
 
+// 判断 string 类型
+export function isString(v) {
+  return typeof v === 'string'
+}
+
 // http 链接转化成 https
 export function toHttps(url) {
+  if (!isString(url)) {
+    return url
+  }
   return url.replace('http://', 'https://')
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const dayjs = require('dayjs')
 
 function resolve(dir) {
   return path.join(__dirname, dir)
@@ -8,6 +9,10 @@ const isEnvProduction = process.env.NODE_ENV === 'production'
 
 // 注入版本信息
 process.env.VUE_APP_VERSION = require('./package.json').version
+// 注入版本更新时间
+process.env.VUE_APP_UPDATE_TIME = dayjs()
+  .locale('zh-cn')
+  .format('YYYY-MM-DD')
 
 module.exports = {
   publicPath: '',
