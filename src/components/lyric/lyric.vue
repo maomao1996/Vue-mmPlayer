ebb9914dfdce071d6d15e45c16cccf1eb3cf8e8f
Pochinki98 <109138085+Pochinki98@users.noreply.github.com>
2023-06-06 19:24:51 +0800

    Update lyric.vue

--- src/components/lyric/lyric.vue
+++ src/components/lyric/lyric.vue
@@ -11,7 +11,7 @@
         <dd>专辑名：{{ currentMusic.album }}</dd>
       </template>
       <template v-else>
-        <dd>mmPlayer在线音乐播放器</dd>
+        <dd>在线音乐播放器</dd>
         <dd>
           <a class="hover" target="_blank" href="https://ygwiki.net">
             <mm-icon type="github" :size="14" />
